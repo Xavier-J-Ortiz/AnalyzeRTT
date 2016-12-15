e39fc9f51198be2d52c85457a151c390ce125e00
Xavier Ortiz <cuchumino@gmail.com>
2016-12-15 13:35:19 -0500

    added some comments in python. On a side note, Realizing I'm a bit rusty in python and in a pythonic way. Too much Javascript recently!

--- analysisRTT.py
+++ analysisRTT.py
@@ -1,10 +1,24 @@
+'''
+Parse S3 logs loaded on the local file system. Do some math.
+Analyze results.
+Currently a moving target by Xavier-J-Ortiz
+'''
 import gzip, os
 
+# functions
+
 def unpack_files():
+    '''
+    unpack_files - grabs a file_path, looks at all files ending in .gz
+    opens them up, prints out content.
+    '''
     file_path = "/home/xortiz/cedexis/S3LogsTest/12"
     actual_content = ""
+
     for file in os.listdir(file_path):
+
         unzipped_file = ""
+
         if file.endswith(".gz"):
             full_file_path = file_path + "/" + file
             unzipped_file = gzip.open(full_file_path, 'rb')
