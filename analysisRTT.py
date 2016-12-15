bb60fe118ffb76b282ef1bda1a97acb27b203c63
Xavier Ortiz <cuchumino@gmail.com>
2016-12-15 15:06:11 -0500

    Added code that will traverse across a specified directory and pick out all the .gz files, then concatenate them. These are done in no particular order, so the resulting string is 'unsorted'. Might need to be sorted in a later iteration.

--- analysisRTT.py
+++ analysisRTT.py
@@ -14,20 +14,31 @@ def unpack_files():
     unpack_files - grabs a file_path, looks at all files ending in .gz
     opens them up, prints out content, saves in test.txt.
     '''
-    file_path = "/home/xortiz/cedexis/S3LogsTest/12"
+
+    ### root file path to traverse for S3 .gz logs
+    ### the algo will traverse across the root file system
+    ### and pick out the .gz logs.
+
+    file_path = "/home/xortiz/cedexis/S3LogsTest"
     actual_content = ""
 
-    for file in os.listdir(file_path):
+    for root, dirs, files in os.walk(file_path):
 
-        unzipped_file = ""
+        ### added these lines below just to see what was being housed in root/dirs/files
+        ### and see what was going on.
+        # print "This is the root: " + root
+        # print "This is the dirs : " + str(dirs)
+        # print "This is the files : " + str(files)
 
-        if file.endswith(".gz"):
-            full_file_path = file_path + "/" + file
-            unzipped_file = gzip.open(full_file_path, 'rb')
-            actual_content += unzipped_file.read()
+        for file in files:
+            unzipped_file = ""
+            if file.endswith(".gz"):
+                full_file_path = root + "/" + file
+                unzipped_file = gzip.open(full_file_path, 'rb')
+                actual_content += unzipped_file.read()
 
-    print actual_content
-
+    ### save actual_content to test.txt
+    ### useful to see if output is getting correctly saved
     # new_test_file = open('test.txt', 'w')
     # new_test_file.write(actual_content)
     # new_test_file.close()
